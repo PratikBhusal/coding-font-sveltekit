b737d91432a3f7b690d5056b01e12b9385cdac5a
PratikBhusal <PratikBhusal@users.noreply.github.com>
2026-06-06 18:44:02 -0700

    Make pnpm run build work again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'monaco-themes/themes': resolve('node_modules/monaco-themes/themes')
+    }
+  },
   server: {
     open: true,
     port: 9000
