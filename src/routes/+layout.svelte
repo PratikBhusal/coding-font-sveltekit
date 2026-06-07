2f969a2886c645c130c67d5e535436eb019843c3
PratikBhusal <PratikBhusal@users.noreply.github.com>
2026-06-06 20:52:16 -0700

    Make the page respect preferred light/dark mode
    Every time you do a page refresh it always goes back to light mode. This
    small change should fix that issue.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 import '../app.css';
+import { setInitialClassState } from '@skeletonlabs/skeleton';
 </script>
+
+<svelte:head>
+  {@html `<script nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
+</svelte:head>
 
 <slot />
